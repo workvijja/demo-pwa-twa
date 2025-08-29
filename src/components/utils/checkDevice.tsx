c446cd9ca77097e56a30c1353386eab426dd3cf5
Vijjasena <work.vijjasena@gmail.com>
2025-08-29 12:02:15 +0700

    fix - revamp flutter x pwa comm way

--- src/components/utils/checkDevice.tsx
+++ src/components/utils/checkDevice.tsx
@@ -26,12 +26,31 @@ export default function CheckDevice({children}: {children: React.ReactNode}) {
       return;
     }
 
-    if (!(channelName in window)) {
-      router.replace('/download');
-      return;
+    window.flutterToPwaChannel = (message: string) => {
+      window.postMessage(message, '*');
     }
 
-    setPermit(true);
+    const checkMessage = (e: MessageEvent) => {
+      if (e.data !== process.env.NEXT_PUBLIC_FLUTTER_SECRET_MESSAGE) {
+        console.error('Invalid message received')
+        router.replace('/download');
+        return;
+      }
+      setPermit(true);
+    }
+
+    window.addEventListener('message', checkMessage);
+    const timeoutId = setTimeout(() => {
+      console.error('Timeout: Flutter message not received');
+      router.replace('/download');
+      window.removeEventListener('message', checkMessage);
+      setPermit(false); // Optional: Set permit to false when timeout occurs
+    }, 10 * 1000);
+
+    return () => {
+      window.removeEventListener('message', checkMessage);
+      clearTimeout(timeoutId);
+    };
   }, []);
 
   if (!permit) return null;
