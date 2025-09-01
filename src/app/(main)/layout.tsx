f9c2b860c03fae6cb95df4fe5b5964bbda346b60
Vijjasena <work.vijjasena@gmail.com>
2025-09-01 14:48:20 +0700

    fix layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import CheckDevice from "@/components/utils/checkDevice";
 import {FlutterBridgeProvider} from "@/provider/flutterBridgeProvider";
 
 export default function MainLayout({children}: {children: React.ReactNode}) {
-  // return children;
+  return children;
   // return (
   //   <FlutterBridgeProvider>
   //     <CheckDevice>
