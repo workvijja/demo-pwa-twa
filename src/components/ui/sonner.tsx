964bfabdd48303f172de7b731727658bf3722173
Vijjasena <work.vijjasena@gmail.com>
2025-09-11 21:07:31 +0700

    fix toaster position on mobile

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -2,14 +2,17 @@
 
 import { useTheme } from "next-themes"
 import { Toaster as Sonner, ToasterProps } from "sonner"
+import {useIsMobile} from "@/hooks/use-mobile";
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
+  const isMobile = useIsMobile();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position={isMobile ? "top-center" : "bottom-right"}
       style={
         {
           "--normal-bg": "var(--popover)",
