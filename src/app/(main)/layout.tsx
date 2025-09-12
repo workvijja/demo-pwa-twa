c09ce630522e1b420fd3b519495a1bd2fb9ac4ab
Vijjasena <work.vijjasena@gmail.com>
2025-09-12 15:09:20 +0700

    create hoc for component feature flag - create check device wrapper

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,16 +5,22 @@ import QueryProvider from "@/provider/queryProvider";
 // import {AuthProvider} from "@/provider/authProvider";
 import Header from "@/app/(main)/_feature/header";
 
+const componentAllowed = <T extends object>(allowed: boolean, Comp: React.FC<T>) =>
+  allowed ? Comp : ({children}: {children: React.ReactNode}) => <>{children}</>;
+
+const CheckDeviceWrapper = componentAllowed(
+  process.env.NEXT_PUBLIC_ENABLE_CHECK_DEVICE === "true",
+  CheckDevice
+);
+
 export default function MainLayout({children}: {children: React.ReactNode}) {
   return (
     <QueryProvider>
       <FlutterBridgeProvider>
-        <CheckDevice>
-          {/*<AuthProvider>*/}
-            <Header/>
-            {children}
-          {/*</AuthProvider>*/}
-        </CheckDevice>
+        <CheckDeviceWrapper>
+          <Header/>
+          {children}
+        </CheckDeviceWrapper>
       </FlutterBridgeProvider>
       <UpdateNotifier/>
     </QueryProvider>
