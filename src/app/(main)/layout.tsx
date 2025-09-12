07a4c4f1f7a29194763c0d17f1ebe955ce548ebc
Vijjasena <work.vijjasena@gmail.com>
2025-09-12 15:24:44 +0700

    create hoc for component feature flag - create check device wrapper

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import CheckDevice from "@/components/utils/checkDevice";
 import {FlutterBridgeProvider} from "@/provider/flutterBridgeProvider";
 import {UpdateNotifier} from "@/components/utils/updateNotifier";
