4aa2bf400abed351a75c0d33787a932c38852643
Vijjasena <work.vijjasena@gmail.com>
2025-09-12 15:21:03 +0700

    create hoc for component feature flag - create check device wrapper

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,6 @@ import CheckDevice from "@/components/utils/checkDevice";
 import {FlutterBridgeProvider} from "@/provider/flutterBridgeProvider";
 import {UpdateNotifier} from "@/components/utils/updateNotifier";
 import QueryProvider from "@/provider/queryProvider";
-// import {AuthProvider} from "@/provider/authProvider";
 import Header from "@/app/(main)/_feature/header";
 
 const componentAllowed = <T extends object>(allowed: boolean, Comp: React.FC<T>) =>
@@ -10,18 +9,22 @@ const componentAllowed = <T extends object>(allowed: boolean, Comp: React.FC<T>)
 
 const CheckDeviceWrapper = componentAllowed(
   process.env.NEXT_PUBLIC_ENABLE_CHECK_DEVICE === "true",
-  CheckDevice
+  ({children}: {children: React.ReactNode}) => (
+    <FlutterBridgeProvider>
+      <CheckDevice>
+        {children}
+      </CheckDevice>
+    </FlutterBridgeProvider>
+  )
 );
 
 export default function MainLayout({children}: {children: React.ReactNode}) {
   return (
     <QueryProvider>
-      <FlutterBridgeProvider>
-        <CheckDeviceWrapper>
-          <Header/>
-          {children}
-        </CheckDeviceWrapper>
-      </FlutterBridgeProvider>
+      <CheckDeviceWrapper>
+        <Header/>
+        {children}
+      </CheckDeviceWrapper>
       <UpdateNotifier/>
     </QueryProvider>
   )
